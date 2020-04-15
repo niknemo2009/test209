83cebb315b5bf277567d5029f3ba6ec940a366bf
Yankovyi_M <maks.yankovyi@gmail.com>
2020-04-15 14:55:15 +0300

    first commit

--- src/Proba.java
+++ src/Proba.java
@@ -1,2 +1,18 @@
-public class Proba {
+public class Zver {
+
+    public class  Exampel1 {
+
+        public static void main(String[] args) {
+
+        }
+        void modifyArrayWithInterface (int[] arr, Algoritmic fun){
+
+        }
+
+        void modify(int[]arr,int koef,int digit){
+            for (int i = 0;i <arr.length; i++){
+                arr[i]=arr [i]*3+2;
+            }
+        }
+    }
 }
